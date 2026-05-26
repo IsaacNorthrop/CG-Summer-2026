08e9c22775385f07ea050b6bec6e4f096eb2c79a
Isaac Northrop <isaacn8@node-1w7jr9ssd3eg082cy5omu8lit.ipv6.telus.net>
2026-05-25 17:52:17 -0700

    solution

--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -17,29 +17,48 @@ const std::string root_path = DATA_DIR;
 // Computes the determinant of the matrix whose columns are the vector u and v
 double inline det(const Vector2d &u, const Vector2d &v)
 {
-    // TODO
-    return 0;
+    return u.x() * v.y() - u.y() * v.x();
 }
 
 // Return true iff [a,b] intersects [c,d]
 bool intersect_segment(const Vector2d &a, const Vector2d &b, const Vector2d &c, const Vector2d &d)
 {
-    // TODO
-    return true;
+    double d1 = det(b - a, c - a);
+    double d2 = det(b - a, d - a);
+    double d3 = det(d - c, a - c);
+    double d4 = det(d - c, b - c);
+
+    return (d1 * d2 <= 0) && (d3 * d4 <= 0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 bool is_inside(const std::vector<Vector2d> &poly, const Vector2d &query)
 {
-    std::float max_x, max_y;
-    max_x = max_y = std::numeric_limits<float>::lowest();
+    double max_x, max_y;
+    max_x = max_y = std::numeric_limits<double>::lowest();
     for (const Vector2d &p : poly)    {
         max_x = std::max(max_x, p.x());
         max_y = std::max(max_y, p.y());
     }
     Vector2d far_point(max_x + 1, max_y + 1);
-    return true;
+    int count = 0;
+    for (size_t i = 0; i < poly.size(); ++i)
+    {
+        if (intersect_segment(poly[i], poly[(i + 1) % poly.size()], query, far_point))
+        {
+            count++;
+        }
+    }
+    std::cout << "count: " << count << std::endl;
+    if (count % 2 == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
